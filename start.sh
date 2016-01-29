09ca984613c63326f12d5b0ba95c477dc564d3d3
AlexTong <alexanderytong@gmail.com>
2016-01-29 05:23:07 +0100

    changes to start file to account for new arduino device

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-
-
-docker run -it -p 8888:8888 -v $PWD/data:/root/data rpi-trst
+#!/bin/bash
+docker run -it -p 8888:8888 \
+    -v $PWD/data:/root/data \
+    --device="/dev/ttyACM0:/dev/ttyACM0" \
+    atong01/rpi-trst
