ea377e4eab709b6c929bc8da93699fd771e38208
AlexTong <alexanderytong@gmail.com>
2016-01-30 04:13:58 +0100

    start.sh deals with the correct number of arduinos

--- start.sh
+++ start.sh
@@ -3,10 +3,25 @@ device0="/dev/ttyACM0"
 device1="/dev/ttyACM1"
 if [ -e "$device0" ]
 then
+    if [ -e "$device1" ]
+    then
+        docker run -it -p 8888:8888 \
+            -v $PWD/data:/root/data \
+            -v $PWD/trst:/root/trst \
+            --device="/dev/ttyACM1:/dev/ttyACM1" \
+            --device="/dev/ttyACM0:/dev/ttyACM0" \
+            atong01/rpi-trst-base
+    else
+        docker run -it -p 8888:8888 \
+            -v $PWD/data:/root/data \
+            -v $PWD/trst:/root/trst \
+            --device="/dev/ttyACM0:/dev/ttyACM0" \
+            atong01/rpi-trst-base
+    fi
+else
     docker run -it -p 8888:8888 \
         -v $PWD/data:/root/data \
         -v $PWD/trst:/root/trst \
         --device="/dev/ttyACM0:/dev/ttyACM0" \
         atong01/rpi-trst-base
 fi
-echo "Device 0 not found at /dev/ttyACM0"
